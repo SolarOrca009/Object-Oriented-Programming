9bcdaa8843a4f4c7388d791508706b36947b1d37
SolarOrca009 <joseeddiecortez@gmail.com>
2026-08-26 22:56:01 -0700

    Completed hello.cpp file
    Finished the code

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*
+ * This lab is an intro, firstly to warm up our skills in coding, and also to learn how to make a github and how it would work.
+ * While we did set up a github account and stuff, we still have yet to actually utilize its version-saving capabilities.
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	printf("Hello World again!\n");
+	return 0;
+}
